5a1ee1cc6f74bb9b6555a8a668bf0ae2574e3fd2
Juan Belich <jbelich@gmail.com>
2024-06-19 09:42:20 -0300

    Reglas de juego

--- avatar.js
+++ avatar.js
@@ -1,34 +1,87 @@
-let botonPersonaje = document.getElementById('boton-personaje');
-botonPersonaje.addEventListener('click', seleccionarPersonaje);
+let ataqueJugador
+let ataqueEnemigo
 
-function seleccionarPersonaje() {
-  let personajeSeleccionado = document.querySelector('input[name="personaje"]:checked');
-  if (personajeSeleccionado) {
-    let nombrePersonaje = personajeSeleccionado.id;
-    alert('Seleccionaste al personaje: ' + nombrePersonaje);
-    document.getElementById("personaje-jugador").innerHTML=nombrePersonaje
-  } else {
-    alert('Por favor, selecciona un personaje antes de continuar.');
-  }
+function iniciarJuego(){
+    let botonPersonajeJugador = document.getElementById('boton-personaje');
+    botonPersonajeJugador.addEventListener('click', seleccionarPersonajeJugador);
+
+    let botonPunio = document.getElementById('boton-punio') //Ahora creamos un escuchador de eventos
+    botonPunio.addEventListener('click', ataquePunio)
+    let botonPatada = document.getElementById('boton-patada')
+    botonPatada.addEventListener('click', ataquePatada)
+    let botonBarrida = document.getElementById('boton-barrida')
+    botonBarrida.addEventListener('click', ataqueBarrida)
+
 }
 
-function personajePC(){
-  let random = Math.floor(Math.random() * (0,4));
-  switch (random) {
-      case 0:
-        document.getElementById("personaje-enemigo").innerHTML="Aang"
-      break;
+function seleccionarPersonajeJugador(){
+    let inputZuko = document.getElementById('zuko')
+    let inputKatara = document.getElementById('katara')
+    let inputAang = document.getElementById('aang')
+    let inputToph = document.getElementById('toph')
+    let spanPersonajeJugador = document.getElementById('personaje-jugador')
 
-      case 1:
-        document.getElementById("personaje-enemigo").innerHTML="Zuko"
-      break;
-
-      case 2:
-        document.getElementById("personaje-enemigo").innerHTML="Katara"
-      break;
-      
-      case 3:
-        document.getElementById("personaje-enemigo").innerHTML="Toph"  
-      break;
-  }
+    if(inputZuko.checked){
+        spanPersonajeJugador.innerHTML = 'Zuko'
+    }else if(inputKatara.checked){
+        spanPersonajeJugador.innerHTML = 'Katara'
+    }else if(inputAang.checked){
+        spanPersonajeJugador.innerHTML = 'Aang'
+    }else if(inputToph.checked){
+        spanPersonajeJugador.innerHTML = 'Toph'
+    }else{
+        alert('Selecciona un personaje')
+    }
+    seleccinarPersonajeEnemigo()
 }
+
+function seleccinarPersonajeEnemigo(){ //esta función va dentro de seleccionarPersonajeJugador() al final
+    let personajeAleatorio = aleatorio(1, 4) //A continuación creamos las variables para cada personaje
+    let spanPersonajeEnemigo = document.getElementById('personaje-enemigo')
+
+    //comenzamos con la lógica
+    if(personajeAleatorio == 1){
+        spanPersonajeEnemigo.innerHTML = 'Zuko'
+    } else if(personajeAleatorio == 2){
+        spanPersonajeEnemigo.innerHTML = 'Katara'
+    } else if(personajeAleatorio == 3){
+        spanPersonajeEnemigo.innerHTML = 'Aang'
+    } else {
+        spanPersonajeEnemigo.innerHTML = 'Toph'
+    }
+}
+
+function ataquePunio(){ //Modificamos la variable global ataqueJugador
+    ataqueJugador = 'Punio'
+    ataqueAleatorioEnemigo()
+}
+
+function ataquePatada(){ //Modificamos la variable global ataqueJugador
+    ataqueJugador = 'Patada'
+    ataqueAleatorioEnemigo()
+}
+
+function ataqueBarrida(){ //Modificamos la variable global ataqueJugador
+    ataqueJugador = 'Barrida'
+    ataqueAleatorioEnemigo()
+}
+
+function ataqueAleatorioEnemigo(){//Ahora ocupando la variable global nueva le decimos el ataque y necesitamos la función aleatorio
+    let ataqueAleatorio = aleatorio(1, 3)
+   
+    if(ataqueAleatorio == 1){
+        ataqueEnemigo = 'Punio'
+    } else if(ataqueAleatorio == 2){
+        ataqueEnemigo = 'Patada'
+    } else {
+        ataqueEnemigo = 'Barrida'
+    }
+}
+
+
+
+function aleatorio(min, max){
+    return Math.floor( Math.random() * (max - min +1) + min)
+}
+
+window.addEventListener('load', iniciarJuego)
